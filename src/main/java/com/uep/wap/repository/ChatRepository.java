71562a909c9c9a552cd8bed2e0976f6db9e85b4a
marsznatalia <marsznatalia@gmail.com>
2024-05-03 13:51:22 +0200

    Repositories ID changed from Integer to Long

--- src/main/java/com/uep/wap/repository/ChatRepository.java
+++ src/main/java/com/uep/wap/repository/ChatRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRepository extends CrudRepository<Chat, Integer> {
+public interface ChatRepository extends CrudRepository<Chat, Long> {
 }
